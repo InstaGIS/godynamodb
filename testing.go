0c01f13437f307636c4754f3d0061649e790d636
Jorge Riquelme <jorge@larix.cl>
2020-01-22 10:20:29 -0300

    CircleCI treated as a special case, the container should be running
    According to
    https://circleci.com/docs/2.0/building-docker-images/#separation-of-environments
    whe can't use setup_remote_docker to run dynamodb-local and access it
    from the tests :(
    
    If the environment variable CIRCLECI=true dockertest isn't used to run
    the container.

--- testing.go
+++ testing.go
@@ -14,7 +14,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/ory/dockertest"
 )
 
@@ -23,44 +22,48 @@ type Test struct {
 	// Table is the name of the table to create.
 	Table string
 
-	endpoint string
+	inCircleCI bool
+	endpoint   string
 }
 
 // TestMain runs the tests.
 //
 // Using this function we can ensure the underlying resources are freed (check https://github.com/golang/go/issues/23404)
 func (t *Test) TestMain(m *testing.M, setupDB func(svc *dynamodb.Client) error) int {
-	// run dynamodb container
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Printf("couldn't connect to docker: %s", err)
-		return 1
-	}
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		ExposedPorts: []string{"8000"},
-		Repository:   "amazon/dynamodb-local",
-	})
-	if err != nil {
-		log.Printf("couldn't start container: %s", err)
-		return 1
-	}
-	defer pool.Purge(resource)
+	t.inCircleCI = os.Getenv("CIRCLECI") == "true"
+	switch t.inCircleCI {
+	case false:
+		// run dynamodb container
+		pool, err := dockertest.NewPool("")
+		if err != nil {
+			log.Printf("couldn't connect to docker: %s", err)
+			return 1
+		}
+		resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+			ExposedPorts: []string{"8000"},
+			Repository:   "amazon/dynamodb-local",
+		})
+		if err != nil {
+			log.Printf("couldn't start container: %s", err)
+			return 1
+		}
+		defer pool.Purge(resource)
 
-	// wait for dynamodb to be ready
-	// FIXME: debug info, remove later
-	spew.Dump(resource.Container.NetworkSettings)
-	switch os.Getenv("DOCKER_HOST") {
-	case "":
 		t.endpoint = resource.GetHostPort("8000/tcp")
+		log.Printf("endpoint: %s", t.endpoint)
+
+		// wait for dynamodb to be ready
+		err = pool.Retry(t.waitForDynamoDB)
+		if err != nil {
+			log.Printf("could not connect to dynamodb: %s", err)
+			return 1
+		}
 	default:
-		t.endpoint = resource.Container.NetworkSettings.Networks["bridge"].IPAddress + ":8000"
+		// CircleCI spins up the container for us
+		t.endpoint = "localhost:8000"
+		log.Printf("endpoint: %s", t.endpoint)
 	}
-	log.Printf("endpoint: %s", t.endpoint)
-	err = pool.Retry(t.waitForDynamoDB)
-	if err != nil {
-		log.Printf("could not connect to dynamodb: %s", err)
-		return 1
-	}
+
 	// setup database
 	svc, err := t.GetClient()
 	if err != nil {
