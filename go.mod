98dc606943e45c326e4dcdc7b3df885fbbbb9711
Jorge Riquelme <jriquelme@predictablemedia.com>
2020-06-19 10:43:49 -0400

    Replaces github.com/ory/dockertest by github.com/testcontainers/testcontainers-go

--- go.mod
+++ go.mod
@@ -3,23 +3,16 @@ module github.com/InstaGIS/godynamodb
 go 1.13
 
 require (
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/Microsoft/go-winio v0.4.14 // indirect
-	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/aws/aws-sdk-go-v2 v0.22.0
-	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
-	github.com/containerd/continuity v0.0.0-20190827140505-75bee3e2ccb6 // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/containerd/continuity v0.0.0-20200413184840-d3ef23f19fbb // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/google/uuid v1.1.1
-	github.com/gotestyourself/gotestyourself v2.2.0+incompatible // indirect
-	github.com/lib/pq v1.2.0 // indirect
-	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
-	github.com/opencontainers/image-spec v1.0.1 // indirect
-	github.com/opencontainers/runc v0.1.1 // indirect
-	github.com/ory/dockertest v3.3.5+incompatible
-	github.com/sirupsen/logrus v1.4.2 // indirect
-	github.com/stretchr/testify v1.4.0
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/stretchr/testify v1.6.1
+	github.com/testcontainers/testcontainers-go v0.7.0
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
 )
--- testing.go
+++ testing.go
@@ -4,18 +4,21 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
-
-	"github.com/aws/aws-sdk-go-v2/aws/retry"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/awserr"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/ory/dockertest"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 // Test is a DynamoDB integration test that runs using a local Docker container.
@@ -35,30 +38,30 @@ func (t *Test) TestMain(m *testing.M, setupDB func(svc *dynamodb.Client) error) 
 	switch t.inCircleCI {
 	case false:
 		// run dynamodb container
-		pool, err := dockertest.NewPool("")
+		port, err := getFreePort()
 		if err != nil {
-			log.Printf("couldn't connect to docker: %s", err)
+			log.Printf("couldn't get a free port: %s", err)
 			return 1
 		}
-		resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-			ExposedPorts: []string{"8000"},
-			Repository:   "amazon/dynamodb-local",
+		ctx := context.Background()
+		req := testcontainers.ContainerRequest{
+			Image:       "amazon/dynamodb-local",
+			Entrypoint:  []string{"java", "-jar", "DynamoDBLocal.jar", "-inMemory", "-port", strconv.Itoa(port)},
+			WaitingFor:  wait.NewLogStrategy("CorsParams"),
+			AutoRemove:  true,
+			NetworkMode: "host",
+		}
+		dynamoDBLocal, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
 		})
 		if err != nil {
 			log.Printf("couldn't start container: %s", err)
 			return 1
 		}
-		defer pool.Purge(resource)
-
-		t.endpoint = resource.GetHostPort("8000/tcp")
+		defer dynamoDBLocal.Terminate(ctx)
+		t.endpoint = "localhost:" + strconv.Itoa(port)
 		log.Printf("endpoint: %s", t.endpoint)
-
-		// wait for dynamodb to be ready
-		err = pool.Retry(t.waitForDynamoDB)
-		if err != nil {
-			log.Printf("could not connect to dynamodb: %s", err)
-			return 1
-		}
 	default:
 		// CircleCI spins up the container for us
 		t.endpoint = "localhost:8000"
@@ -126,4 +129,26 @@ func (t *Test) waitForDynamoDB() error {
 		return err
 	}
 	return nil
+}
+
+func getFreePort() (int, error) {
+	l, err := net.Listen("tcp4", ":0")
+	if err != nil {
+		return 0, fmt.Errorf("couldn't get a free tcp4 port: %s", err)
+	}
+	log.Printf("%s", l.Addr())
+	sep := strings.LastIndex(l.Addr().String(), ":")
+	if sep == -1 {
+		return 0, fmt.Errorf("invalid address, ':' separator not found: %s", l.Addr())
+	}
+	port := l.Addr().String()[sep+1:]
+	err = l.Close()
+	if err != nil {
+		return 0, fmt.Errorf("error closing listener: %s", err)
+	}
+	n, err := strconv.Atoi(port)
+	if err != nil {
+		return 0, fmt.Errorf("invalid port %s: %s", port, err)
+	}
+	return n, nil
 }
